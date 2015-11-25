239c03be5351b1cbaa8da9d46300d52e152cb97e
Angelika Tyborska <atyborska93@gmail.com>
2015-11-25 15:24:43 +0100

    Add subjects to teachers routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     get :subjects
   end
 
-  resources :teachers
+  resources :teachers do
+    get :subjects
+  end
 
   get 'reports/subjects', to: 'reports#subjects'
   get 'visitors', to: 'visitors#index'
