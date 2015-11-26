bf617fd82abfcb78aad2ecdda9d1555e6613440b
Angelika Tyborska <atyborska93@gmail.com>
2015-11-26 18:29:21 +0100

    Refactor payment to use decent exposure

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,74 +1,31 @@
 class PaymentsController < ApplicationController
-  before_action :set_payment, only: [:show, :edit, :update, :destroy]
+  expose(:payments)
+  expose(:payment, attributes: :payment_params)
 
-  # GET /payments
-  # GET /payments.json
-  def index
-    @payments = Payment.all
-  end
-
-  # GET /payments/1
-  # GET /payments/1.json
-  def show
-  end
-
-  # GET /payments/new
-  def new
-    @payment = Payment.new
-  end
-
-  # GET /payments/1/edit
-  def edit
-  end
-
-  # POST /payments
-  # POST /payments.json
   def create
-    @payment = Payment.new(payment_params)
-
-    respond_to do |format|
-      if @payment.save
-        format.html { redirect_to @payment, notice: 'Payment was successfully created.' }
-        format.json { render :show, status: :created, location: @payment }
-      else
-        format.html { render :new }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
+    if payment.save
+      redirect_to payment_path(payment), notice: I18n.t('shared.created', resource: 'Payment')
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /payments/1
-  # PATCH/PUT /payments/1.json
   def update
-    respond_to do |format|
-      if @payment.update(payment_params)
-        format.html { redirect_to @payment, notice: 'Payment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @payment }
-      else
-        format.html { render :edit }
-        format.json { render json: @payment.errors, status: :unprocessable_entity }
-      end
+    if payment.save
+      redirect_to payment_path(payment), notice: I18n.t('shared.updated', resource: 'Payment')
+    else
+      render :edit
     end
   end
 
-  # DELETE /payments/1
-  # DELETE /payments/1.json
   def destroy
-    @payment.destroy
-    respond_to do |format|
-      format.html { redirect_to payments_url, notice: 'Payment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    payment.destroy
+    redirect_to payments, notice: I18n.t('shared.deleted', resource: 'Payment')
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_payment
-      @payment = Payment.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def payment_params
-      params.require(:payment).permit(:amount, :student_id, :date)
-    end
+  def payment_params
+    params.require(:payment).permit(:amount, :student_id, :date)
+  end
 end
