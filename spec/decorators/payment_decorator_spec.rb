399c76400fcde21bed64291e97adc41a6b97a9d9
Angelika Tyborska <atyborska93@gmail.com>
2015-11-27 14:24:33 +0100

    Add payment decorator spec

--- spec/decorators/payment_decorator_spec.rb
+++ spec/decorators/payment_decorator_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe PaymentDecorator do
+  let(:student) { create :student }
+  let(:payment) { create :payment, amount: 45, student: student}
+
+  describe "#formatted_amount" do
+    subject { payment.decorate.formatted_amount }
+    it { is_expected.to eq '$45.00' }
+  end
 end
