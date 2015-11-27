19ed7d6d9b430add140e4e97e49e2140f3d58c94
Angelika Tyborska <atyborska93@gmail.com>
2015-11-27 10:29:49 +0100

    Add date and amount validation to payment spec

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe Payment do
     it { is_expected.to validate_presence_of :student }
     it { is_expected.to validate_presence_of :amount }
     it { is_expected.to validate_presence_of :date }
+
+    context 'with a date in the future' do
+      let(:student) { create :student }
+      subject { build :payment, amount: 40, student: student, date: Date.today + 1000 }
+
+      it { is_expected.to_not be_valid }
+    end
+
+    context 'with a non-positive amount' do
+      let(:student) { create :student }
+      subject { build :payment, amount: 0, student: student, date: Date.today }
+
+      it { is_expected.to_not be_valid }
+    end
   end
 
   describe 'database columns' do
